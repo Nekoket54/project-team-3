d2f2835b4cf827f2f09748c6687b7eb19770a6da
Nekoket54 <153765535+Nekoket54@users.noreply.github.com>
2025-02-18 17:18:24 +0200

    AboutGameVideo

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+const box = document.querySelector('.video-box')
+const btn = document.querySelector('.video-box-btn')
+
+btn.addEventListener('click',()=>{
+  box.innerHTML = `
+    <iframe width="1920px" height="944px" 
+      src="https://www.youtube.com/embed/hfJ4Km46A-0?autoplay=1" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      allow="autoplay" 
+      allowfullscreen>
+    </iframe>
+  `
+})
+
+
+
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   loop: true,
