5f7da79a5a920e009f9ce3a2fc6f10ae3b35dcff
Nekoket54 <153765535+Nekoket54@users.noreply.github.com>
2025-03-11 18:10:48 +0200

    2 section and adaptation 60%

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ burger.addEventListener('click',()=>{
 const btn1 = document.querySelector('.edition-container-box1-box-btn')
 const btn2 = document.querySelector('.edition-container-box2-box-btn')
 const btn3 = document.querySelector('.edition-container-box3-box-btn')
+const btn4 = document.querySelector('.dualSense-container-box1-case-btn')
 const FormOfPayment = document.querySelector('.container')
 const close = document.querySelector('.container-close')
 const sum = document.querySelector('.container-box3-text')
@@ -38,6 +39,14 @@ btn3.addEventListener('click',()=>{
   FormOfPayment.classList.replace('FormOfPaymentHidden', 'FormOfPaymentVisible')
   sum.textContent = '29.99$'
   edition.textContent = 'DIGITAL DELUXE EDITION'
+})
+close.addEventListener('click',()=>{
+  FormOfPayment.classList.replace('FormOfPaymentVisible', 'FormOfPaymentHidden')
+})
+btn4.addEventListener('click',()=>{
+  FormOfPayment.classList.replace('FormOfPaymentHidden', 'FormOfPaymentVisible')
+  sum.textContent = '35.99$'
+  edition.textContent = 'DUALSENSE WITH A GAME'
 })
 close.addEventListener('click',()=>{
   FormOfPayment.classList.replace('FormOfPaymentVisible', 'FormOfPaymentHidden')
@@ -62,6 +71,73 @@ btn.addEventListener('click',()=>{
 })
 ////////////////////////////////
 
+//////////////////////////////// Explore the God of War series
+const toggle = document.getElementById("toggle");
+const leftText = document.getElementById("left");
+const rightText = document.getElementById("right");
+const cpu = document.querySelector(".toggle-position-props-paragraph-cpu")
+const ram = document.querySelector(".toggle-position-props-paragraph-ram")
+const os = document.querySelector(".toggle-position-props-paragraph-os")
+const videocard = document.querySelector(".toggle-position-props-paragraph-videocard")
+const pixel = document.querySelector(".toggle-position-props-paragraph-pixel")
+const vertext = document.querySelector(".toggle-position-props-paragraph-vertext")
+const freespace = document.querySelector(".toggle-position-props-paragraph-freespace")
+const dedicated = document.querySelector(".toggle-position-props-paragraph-dedicated")
+
+toggle.addEventListener("change", function () {
+    if (this.checked) {
+        leftText.textContent = "Minimum";
+        rightText.textContent = "Recommended";
+        os.innerHTML =  "<p class='toggle-position-props-paragraph toggle-position-props-paragraph-os'><span class='toggle-position-props-paragraph-span'>OS</span>: Windows 11 64-bit</p>"
+        cpu.innerHTML = "<p> <span class='toggle-position-props-paragraph-span'>CPU</span>: AMD Ryzen 3 1200 (4 core 3.1 GHz) (4 core 3.3 GHz)</p>";
+        videocard.innerHTML = "<p class='toggle-position-props-paragraph toggle-position-props-paragraph-videocard'><span class='toggle-position-props-paragraph-span'>VIDEOCARD</span>: AMD R9 290X (4 GB)</p>"
+        dedicated.innerHTML = "<p class='toggle-position-props-paragraph toggle-position-props-paragraph-dedicated'><span class='toggle-position-props-paragraph-span'>DEDICATED VIDEO RAM</span>: 8 GB</p>"
+        leftText.style.transition= "0.3s";
+        leftText.style.color = "#FFFFFF"; 
+        rightText.style.color = "#30373F"; 
+        leftText.style.color = "#30373F"; 
+        rightText.style.color = "#FFFFFF";
+        rightText.style.transition= "0.3s";
+    } else {
+        leftText.style.color = "#30373F"; 
+        rightText.style.color = "#FFFFFF";
+        rightText.style.transition= "0.3s";
+        os.innerHTML =  "<p class='toggle-position-props-paragraph toggle-position-props-paragraph-os'><span class='toggle-position-props-paragraph-span'>OS</span>: Windows 10 64-bit</p>"
+        cpu.innerHTML = "<p> <span class='toggle-position-props-paragraph-span'>CPU</span>: Intel i5-2500k (4 core 3.3 GHz)</p>";
+        videocard.innerHTML = "<p class='toggle-position-props-paragraph toggle-position-props-paragraph-videocard'><span class='toggle-position-props-paragraph-span'>VIDEOCARD</span>: NVIDIA GTX 960 (4 GB)</p>"
+        dedicated.innerHTML = "<p class='toggle-position-props-paragraph toggle-position-props-paragraph-dedicated'><span class='toggle-position-props-paragraph-span'>DEDICATED VIDEO RAM</span>: 4 GB</p>"
+        leftText.style.transition= "0.3s";
+        leftText.style.color = "#FFFFFF"; 
+        rightText.style.color = "#30373F"; 
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const toggler = document.getElementById("kratos-toggle");
+    const limitedLabel = document.querySelector(".kratos-label-right");
+    const labeler = document.querySelector(".kratos-label-left");
+    const voice = document.querySelector(".toggle-kratos-position-props-paragraph-voice");
+    const premiere = document.querySelector(".toggle-kratos-position-props-paragraph-premiere");
+    const platform = document.querySelector(".toggle-kratos-position-props-paragraph-platform");
+
+    toggler.addEventListener("change", function () {
+        if (toggler.checked) {
+            labeler.style.color = "#30373f"
+            limitedLabel.style.color = "#FFF";
+            platform.innerHTML = "<p class='toggle-kratos-position-props-paragraph toggle-kratos-position-props-paragraph-platform'><span class='toggle-kratos-position-props-paragraph-span'>Platform</span>: PS4, PS5</p>"
+            premiere.innerHTML = "<p class='toggle-kratos-position-props-paragraph toggle-kratos-position-props-paragraph-premiere'><span class='toggle-kratos-position-props-paragraph-span'>Premiere</span>: 19.9.1289</p>"
+            voice.innerHTML = "<p class='toggle-kratos-position-props-paragraph toggle-kratos-position-props-paragraph-voice'><span class='toggle-kratos-position-props-paragraph-span'>Voice</span>: English, Polish, (not)Russian</p>"
+            
+        } else {
+            labeler.style.color = "#FFF"
+            limitedLabel.style.color = "#30373f";
+            platform.innerHTML = "<p class='toggle-kratos-position-props-paragraph toggle-kratos-position-props-paragraph-platform'><span class='toggle-kratos-position-props-paragraph-span'>Platform</span>: PS4</p>"
+            premiere.innerHTML = "<p class='toggle-kratos-position-props-paragraph toggle-kratos-position-props-paragraph-premiere'><span class='toggle-kratos-position-props-paragraph-span'>Premiere</span>: 20.4.2018</p>"
+            voice.innerHTML = "<p class='toggle-kratos-position-props-paragraph toggle-kratos-position-props-paragraph-voice'><span class='toggle-kratos-position-props-paragraph-span'>Voice</span>: English</p>"
+        }
+    });
+});
+////////////////////////////////
 
 //////////////////// slider swiper
 const swiper = new Swiper('.swiper', {
@@ -88,8 +164,8 @@ const swiper = new Swiper('.swiper', {
 //////////////////////
 
 ////////////////////// FAQ
-const btn4 = document.querySelector('.faq-container-box1-case-btn')
-const btn5 = document.querySelector('.faq-container-box2-case-btn')
+const btn5 = document.querySelector('.faq-container-box1-case-btn')
+const btn6 = document.querySelector('.faq-container-box2-case-btn')
 const text1 = document.querySelector('.faq-container-box1-text')
 const text2 = document.querySelector('.faq-container-box2-text')
 const spanFaqBtn1 = document.querySelector('.spanFaqBtn1')
@@ -97,7 +173,7 @@ const spanFaqBtn2 = document.querySelector('.spanFaqBtn2')
 const case1 = document.querySelector('.faq-container-box1-case')
 const case2 = document.querySelector('.faq-container-box2-case')
 
-btn4.addEventListener('click',()=>{
+btn5.addEventListener('click',()=>{
   const check2 = text1.classList.contains('faqHidden')
   if (check2 == true){
     text1.classList.replace('faqHidden','faqVisible')
@@ -110,7 +186,7 @@ btn4.addEventListener('click',()=>{
   }
 })
 
-btn5.addEventListener('click',()=>{
+btn6.addEventListener('click',()=>{
   const check2 = text2.classList.contains('faqHidden')
   if (check2 == true){
     text2.classList.replace('faqHidden','faqVisible')
@@ -122,9 +198,4 @@ btn5.addEventListener('click',()=>{
     case2.style.marginBottom = '0px'
   }
 })
-
-
-
-
-
 //////////////////////
